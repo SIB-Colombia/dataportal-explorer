863aab2d161944a55d6effb713ab69fd03a3cd9e
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2013-09-30 15:52:32 -0500

    Setting 'desconocido' when data is null

--- public/javascripts/app/models/resumeCount.js
+++ public/javascripts/app/models/resumeCount.js
@@ -2,7 +2,11 @@ define(["knockout"], function(ko) {
 	var resumeCount = function(data) {
 		this.id = data.id || null;
 		this.url = data.url || null;
-		this.name = data.name;
+		if(data.name == "null") {
+			this.name = "Desconocido";
+		} else {
+			this.name = data.name;
+		}
 		this.count = data.count;
 	};
 
