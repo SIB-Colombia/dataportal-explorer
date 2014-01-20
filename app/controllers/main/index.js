94fdaee137e15ab18fd7e41aa2b4184427943fd7
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2014-01-20 15:24:20 -0500

    Doing small text change

--- app/controllers/main/index.js
+++ app/controllers/main/index.js
@@ -189,7 +189,7 @@ exports.convertFromMysqlToMongoDB = function(req, res) {
 	c.connect({
 		host: '127.0.0.1',
 		user: 'root',
-		password: 'password',
+		password: 'h4s1p8k21078!',
 		db: 'dataportal'
 	});
 
